e53f027b5f0c9d1a978abe67b22c9b84e7523dce
Jorge Castello <jorgio.castello@gmail.com>
2020-04-14 09:57:21 -0700

    Implement update / delete

--- datastore/index.js
+++ datastore/index.js
@@ -45,43 +45,61 @@ exports.readAll = (callback) => {
 };
 
 exports.readOne = (id, callback) => {
-  //Original Code:
-  var text = items[id];
-  if (!text) {
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    callback(null, { id, text });
-  }
-
-  // fs.readFile(`${exports.dataDir}/${id}.txt`, 'utf8', (err, file) => {
-  //   if (err) {
-  //     callback(new Error(`No item with id: ${id}`));
-  //   } else {
-  //     callback(null, { id, text: file });
-  //   }
-  // });
+  fs.readFile(`${exports.dataDir}/${id}.txt`, 'utf8', (err, file) => {
+    if (err) {
+      callback(new Error(`No item with id: ${id}`));
+    } else {
+      callback(null, { id, text: file });
+    }
+  });
 };
 
 exports.update = (id, text, callback) => {
-  var item = items[id];
-  if (!item) {
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    items[id] = text;
-    callback(null, { id, text });
-  }
+
+  //Step 1: is to see if the file exists - the read method
+  fs.readFile(`${exports.dataDir}/${id}.txt`, (err, file) => {
+    //Step 2: if it doesn't exist, we should send an error
+    if (err) {
+      callback(new Error(`No item with id: ${id}`));
+    } else {
+      //Step 3: if it does exist, we should write the file with the text
+      fs.writeFile(`${exports.dataDir}/${id}.txt`, text, err => {
+        if (err) {
+          //Step 4: if that isn't successful, we should log the error
+          console.log(err);
+        } else {
+          //Step 5: if it is successful, we should invoke the callback on id and the updated text
+          callback(null, {id, text});
+        }
+      });
+    }
+  });
+
 };
 
 exports.delete = (id, callback) => {
-  var item = items[id];
-  delete items[id];
-  if (!item) {
-    // report an error if item not found
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    callback();
-  }
+
+  // Step 1: see if the file exists
+  fs.readFile(`${exports.dataDir}/${id}.txt`, (err) => {
+    if (err) {
+      // Step 2: if doesn't exist, we should send an error
+      callback(new Error(`No item with id: ${id}`));
+    } else {
+      // Step 3: if it exists, we want to unlink the file
+      fs.unlink(`${exports.dataDir}/${id}.txt`, err =>{
+        if (err) {
+          // Step 4: if it wasn't successful, console.log(error)
+          console.log(err);
+        } else {
+          // Step 5: if it was successful, invoke callback
+          callback();
+        }
+      });
+    }
+  });
 };
+
+
 
 // Config+Initialization code -- DO NOT MODIFY /////////////////////////////////
 
